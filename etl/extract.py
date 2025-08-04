168bf4f34d66b720fe72318d06b8404c753bf30b
gmaheswaranmca <gmaheswaranmca@gmail.com>
2025-08-04 06:54:58 +0530

    emp data engineering project

--- etl/extract.py
+++ etl/extract.py
@@ -1,14 +1,25 @@
 import os
 import pandas as pd 
+import requests
 
-def extract(config):
-    url = config['emp_csv_url']
-    file_name = config['emp_csv_file']
-    # read the CSV file from the URL, save locally
-    if not os.path.exists(file_name):
+def extract_data(config):       
+    if os.path.exists(file_name):
+        # read the CSV file locally
+        url = config['emp_csv_url']
         df = pd.read_csv(url)
-    else:
+        print('Local Employees Data extracted successfully.')
+    elif config['source'] == 'csv':
+        # read the CSV file from the URL, save locally
+        file_name = config['emp_csv_file']
         df = pd.read_csv(file_name)
-
-    # read the CSV file and return the data
+        print('URL Employees Data extracted successfully.')
+    elif config['source'] == 'api':
+        # read API data and save it as csv locally
+        url = config['api_url']
+        response = requests.get(url)
+        employees = response.json()
+        df = pd.DataFrame(employees)
+        print('API Employees Data extracted successfully.')
+    
     return df
+
--- etl/transform.py
+++ etl/transform.py
@@ -7,7 +7,7 @@ def transform(lake_employees_df):
     # Reset null salaries to avg salary
     avg_salary = lake_employees_df['salary'].mean()
     lake_employees_df['salary'] = lake_employees_df['salary'].fillna(avg_salary)
-
+    print('Transformed the employee data by resetting null salaries and calculating department salaries.')
     # Calculate department salaries
     df_department_salaries = lake_employees_df.groupby('department').agg({'salary': 'sum'}).reset_index()
 
